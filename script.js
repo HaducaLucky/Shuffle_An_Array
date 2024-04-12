f326b1f41010c6f0d144e2a3c5d1f5d7ebc0acaf
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-04-12 09:34:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//! Fisher-Yates algorithm 
+
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+shuffle(cards);
+
+console.log(cards);
+
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const random = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[random]] = [array[random], array[i]]
+    }
+}
